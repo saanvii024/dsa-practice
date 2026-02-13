b756ecdcb16856209d6a8b8528e95761c5484e6b
Saanvvi C Deshmukh <saanadeshmukh24@gmail.com>
2026-02-13 12:15:23 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (47.08%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char> st;
+
+        for(char c : s){
+            if(isdigit(c)){
+                st.pop();
+            }
+            else{
+                st.push(c);
+            }
+        }
+
+        string result = "";
+        while(!st.empty()){
+            result += st.top();
+            st.pop();
+        }
+
+        reverse(result.begin(), result.end());
+        return result;   
+    }
+};
