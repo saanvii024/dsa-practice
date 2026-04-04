74876a759748c242963c343a57f5554926492bfd
Saanvvi C Deshmukh <saanadeshmukh24@gmail.com>
2026-04-04 11:40:13 +0530

    Time: 12 ms (31.08%) | Memory: 153.5 MB (94.41%) - LeetSync

--- 1731-even-odd-tree/even-odd-tree.cpp
+++ 1731-even-odd-tree/even-odd-tree.cpp
@@ -1,44 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
 class Solution {
 public:
-    bool check(TreeNode* root) {
-        if (root == NULL) return true;
-
+    bool isEvenOddTree(TreeNode* root) {
         queue<TreeNode*> q;
         q.push(root);
+        int size = 1;
         int level = 0;
-
+        int prev = INT_MIN;
         while (!q.empty()) {
-            int size = q.size();
-            int prev = (level % 2 == 0) ? INT_MIN : INT_MAX;
-
-            for (int i = 0; i < size; i++) {
-                TreeNode* node = q.front();
-                q.pop();
-
+            while (size > 0) {
                 if (level % 2 == 0) {
-                    if (node->val % 2 == 0 || node->val <= prev) {
+                    if (q.front()->val % 2 == 0 || q.front()->val <= prev)
                         return false;
-                    }
-                }
-
-                else {
-                    if (node->val % 2 != 0 || node->val >= prev) {
+                    prev = q.front()->val;
+                } else {
+                    if (q.front()->val % 2 == 1 || q.front()->val >= prev)
                         return false;
-                    }
+                    prev = q.front()->val;
                 }
-
-                prev = node->val;
-
-                if (node->left != nullptr) q.push(node->left);
-                if (node->right != nullptr) q.push(node->right);
+                if (q.front()->left)
+                    q.push(q.front()->left);
+                if (q.front()->right)
+                    q.push(q.front()->right);
+                q.pop();
+                size--;
             }
-            
+            size = q.size();
             level++;
+            prev = (level % 2 == 0) ? INT_MIN : INT_MAX;
         }
         return true;
-    }
-
-    bool isEvenOddTree(TreeNode* root) {
-        return check(root);
     }
 };
