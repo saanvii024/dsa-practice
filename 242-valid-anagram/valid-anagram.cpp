5c9e59b2fde1c707f30831f296f14a7fc2c96a0d
Saanvvi C Deshmukh <saanadeshmukh24@gmail.com>
2026-04-20 16:58:53 +0530

    Time: 2 ms (50.47%) | Memory: 9.7 MB (47.75%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
+        if(s.length() != t.length()){
+            return false;
+        }
+      
         unordered_map <char, int> num;
 
         for(auto x : s){
