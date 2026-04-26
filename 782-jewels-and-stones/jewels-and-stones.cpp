05cea652a295fdc011592a96a8ee9a2f31984372
Saanvvi C Deshmukh <saanadeshmukh24@gmail.com>
2026-04-27 00:37:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (6.41%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.cpp
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
     int numJewelsInStones(string jewels, string stones) {
-        int count =0;
-        for(char j : jewels){
-            for(char s : stones){
-                if ( j==s){
-                    count++;
-                }
+        unordered_map<char,int> m;
+
+        // store all jewels
+        for(char ch : jewels){
+            m[ch] = 1;
+        }
+
+        int cnt = 0;
+
+        // count matching stones
+        for(char ch : stones){
+            if(m[ch] == 1){ // check if stone is a jewel
+                cnt++;
             }
         }
-        return count;
-        
+
+        return cnt;
     }
 };
