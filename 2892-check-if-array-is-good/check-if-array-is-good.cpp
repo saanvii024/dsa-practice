737d3e8593f5fb96e8704cdd355204c88bdea6f2
Saanvvi C Deshmukh <saanadeshmukh24@gmail.com>
2026-05-14 13:12:23 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (97.93%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.cpp
+++ 2892-check-if-array-is-good/check-if-array-is-good.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     bool isGood(vector<int>& nums) {
+
         sort(nums.begin(), nums.end());
+
         int n = nums.size();
-        int maxx = n-1;
-        unordered_map<int, int> mp;
-        for(int i = 0; i < nums.size(); i++){
-            mp[nums[i]]++;
-        }
-        for(int i = 1; i < maxx; i++){
-            if(mp[i]!=1){
+
+        for(int i = 0; i < n-1; i++){
+
+            if(nums[i] != i+1)
                 return false;
-            }
         }
-        return mp[maxx] == 2; //if ==2 it returns true,else false
-        
+
+        return nums[n-1] == n-1;
     }
 };
