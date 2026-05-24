fda1c66fff677e9e2eaf7b4e5927735a51aa05c3
Saanvvi C Deshmukh <saanadeshmukh24@gmail.com>
2026-05-24 14:08:44 +0530

    Time: 11 ms (74.31%) | Memory: 19.1 MB (56.42%) - LeetSync

--- 1466-jump-game-v/jump-game-v.cpp
+++ 1466-jump-game-v/jump-game-v.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
     int dfs(int i, vector<int>& arr, int d, vector<int>& dp) {
+        //memoization
         if (dp[i] != -1)
             return dp[i];
 
+        //min best is 1 visited index
         int best = 1;
         int n = arr.size();
 
+        //going forward and the constraint is that it should not go beyond array and also beyond d
         for (int nxt = i + 1; nxt <= min(n - 1, i + d); nxt++) {
             if (arr[nxt] >= arr[i])
-                break;
+                break; //wall in between 
 
             best = max(best, 1 + dfs(nxt, arr, d, dp));
         }
-
+        //going backward
         for (int nxt = i - 1; nxt >= max(0, i - d); nxt--) {
             if (arr[nxt] >= arr[i])
                 break;
 
             best = max(best, 1 + dfs(nxt, arr, d, dp));
         }
-
+        //returning the best for this particular index
         return dp[i] = best;
     }
 
@@ -29,7 +32,7 @@ public:
         vector<int> dp(n, -1);
 
         int ans = 1;
-
+        //check all starting points
         for (int i = 0; i < n; i++) {
             ans = max(ans, dfs(i, arr, d, dp));
         }
