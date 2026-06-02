f625fb6d9371c6fb402524603e7a3789a90aad70
Saanvvi C Deshmukh <saanadeshmukh24@gmail.com>
2026-06-02 16:19:31 +0530

    Time: 63 ms (57.78%) | Memory: 14.1 MB (74.88%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n,1);
+        int ans = 1;
+        for(int i = 1; i <nums.size(); i++){
+            for(int j = 0; j < i; j++){
+                if(nums[i]> nums[j]){
+                    dp[i] = max(dp[j]+1 , dp[i]);
+
+                }
+                
+            }
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+        //return  *max_element(dp.begin(), dp.end()); //* is a pointer
+    }
+};
