e071dbccdd9f8fc57d61b8c3a5e2547b2ff32f11
Saanvvi C Deshmukh <saanadeshmukh24@gmail.com>
2026-06-18 13:50:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (46.25%) - LeetSync

---
+++ 1446-angle-between-hands-of-a-clock/angle-between-hands-of-a-clock.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double angleClock(int hour, int minutes) {
+        double minAngle = 6.0 * minutes;
+        double hrAngle = 30.0 * (hour % 12) + 0.5 * minutes;
+
+        double diff = abs(hrAngle - minAngle);
+
+        return min(diff, 360.0 - diff);
+    }
+};
+
+//360 degree, 60 minutes, per katta 6 degree, 30x12=360
+//convert min into degree, and find absolute difference, + or - of hours katta in minutes katta
